ca51d2e211a454c845f0344ef5689a8075688222
Eslam Ali <preslamali@hotmail.com>
2022-12-20 16:28:49 +0200

    update

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,52 +1,48 @@
-const fs = require("fs");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const Tour = require("./../../models/tourModel");
+const fs = require('fs')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const Tour = require('./../../models/tourModel')
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
-);
+)
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
   })
-  .then(() => console.log("DB connection successful!"));
+  .then(() => console.log('DB connection successful!'))
 
 // READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
-    await Tour.create(tours);
-    console.log("Data successfully loaded!");
+    await Tour.create(tours)
+    console.log('Data successfully loaded!')
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-  process.exit();
-};
+  process.exit()
+}
 
 // DELETE ALL DATA FROM DB
 const deleteData = async () => {
   try {
-    await Tour.deleteMany();
-    console.log("Data successfully deleted!");
+    await Tour.deleteMany()
+    console.log('Data successfully deleted!')
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-  process.exit();
-};
+  process.exit()
+}
 
-if (process.argv[2] === "--import") {
-  importData();
-} else if (process.argv[2] === "--delete") {
-  deleteData();
+if (process.argv[2] === '--import') {
+  importData()
+} else if (process.argv[2] === '--delete') {
+  deleteData()
 }
